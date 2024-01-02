eadbbf9a54e64e5249a8c00645ebd3b983f540d1
loieee <470618507@qq.com>
2024-01-02 20:46:43 +0800

    initial commit

---
+++ packages/sandbox/src/App.jsx
@@ -0,0 +1,12 @@
+import { Masonry } from '../../ui/src/index'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Masonry />
+    </>
+  )
+}
+
+export default App
---
+++ packages/ui/src/components/masonry/index.jsx
@@ -0,0 +1,5 @@
+export const Masonry = () => (
+  <>
+    Hello masonry...
+  </>
+)
